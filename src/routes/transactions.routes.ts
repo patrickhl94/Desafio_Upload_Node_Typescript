5069e2c7ebdcccbb74faa0201f89496676019053
Patrick Lima <patrickhl94@hotmail.com>
2020-05-02 22:06:04 -0300

    Updated routes transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,22 +1,47 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
+import Transaction from '../models/Transaction';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionRepository = getRepository(Transaction);
+  const transactionsRepository = new TransactionsRepository();
+  const transactions = await transactionRepository.find();
+  const balance = await transactionsRepository.getBalance(transactions);
+
+  return response.json({
+    transactions,
+    balance,
+  });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  const createTransactionService = new CreateTransactionService();
+
+  const transaction = await createTransactionService.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const deleteTransactionService = new DeleteTransactionService();
+  const deleted = await deleteTransactionService.execute(id);
+
+  return response.json({ deleted });
 });
 
 transactionsRouter.post('/import', async (request, response) => {
