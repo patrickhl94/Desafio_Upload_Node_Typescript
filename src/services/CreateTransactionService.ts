4d063fca11dee52eaf2de817cf352813b8fd9546
Patrick Lima <patrickhl94@hotmail.com>
2020-05-02 22:06:41 -0300

    Updated service CreateTransaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,40 @@
+import { getRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import CreateCategoryService from './CreateCategoryService';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    category,
+    type,
+    title,
+    value,
+  }: Request): Promise<Transaction> {
+    const createCategoryService = new CreateCategoryService();
+    const transactionRepository = getRepository(Transaction);
+
+    const categoryCreated = await createCategoryService.execute(category);
+
+    const { id: category_id } = categoryCreated;
+
+    const transaction = transactionRepository.create({
+      category_id,
+      type,
+      title,
+      value,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
