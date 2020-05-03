0ad5d2819a0a31149bcfd54898da7501cc5e067f
Patrick Lima <patrickhl94@hotmail.com>
2020-05-02 22:06:57 -0300

    Updated service DeleteTransaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<boolean> {
+    const transactionRepository = getRepository(Transaction);
+
+    const { affected } = await transactionRepository.delete({ id });
+
+    if (!affected) {
+      throw new AppError('Transaction not found with the given ID');
+    }
+
+    return true;
   }
 }
 
